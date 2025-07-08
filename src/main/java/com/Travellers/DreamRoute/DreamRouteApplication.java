29fcb5b56d30a9087cd1111d8255cc006d6a9a1a
may-leth <mayleth9609@gmail.com>
2025-07-08 13:56:41 +0200

    first commit

---
+++ src/main/java/com/Travellers/DreamRoute/DreamRouteApplication.java
@@ -0,0 +1,13 @@
+package com.Travellers.DreamRoute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamRouteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreamRouteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Travellers/DreamRoute/DreamRouteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Travellers.DreamRoute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamRouteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
