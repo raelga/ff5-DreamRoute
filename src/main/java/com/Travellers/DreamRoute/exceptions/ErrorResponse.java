e4132ebce6c519f98f7cda615e7b06f6fec35100
Nia <niaespinal@outlook.com>
2025-07-15 12:42:11 +0200

    feat(EntityNotFoundException, ErrorResponse, GlobalExceptionHandler): Add EntityNotFoundException methods, handleEntityNotFoundException, handleValidationExceptions

--- src/main/java/com/Travellers/DreamRoute/exceptions/ErrorResponse.java
+++ src/main/java/com/Travellers/DreamRoute/exceptions/ErrorResponse.java
@@ -1,4 +1,20 @@
 package com.Travellers.DreamRoute.exceptions;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
 public class ErrorResponse {
+    String message;
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    LocalDateTime timestamp;
+
+    public ErrorResponse(String message) {
+        this.message = message;
+        this.timestamp = LocalDateTime.now();
+    }
 }
